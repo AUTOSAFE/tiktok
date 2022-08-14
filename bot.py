36552a7542dd4a78700b84a1f80525770bba20bd
AUTOSAFE <107992265+AUTOSAFE@users.noreply.github.com>
2022-08-14 22:14:32 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python 
 # -*- coding: utf-8 -*-
 
-VIDEO = "7126536525008882949" #video id
+VIDEO = "7131653805854280986" #video id
 
 import requests, time, random, bs4, sys, re, base64, urllib.parse
 
